89cc2eaec723ff64fd3baa5c9a70284e54e5e223
Samuel Kupferschmid <samuel.kupferschmid@gmail.com>
2020-03-04 23:51:07 +0100

    start running interference

--- src/data.py
+++ src/data.py
@@ -8,39 +8,62 @@ import tensorflow_io as tfio
 def load_al_trainingset(filename, data_root):
     df = load_csv(filename)
     df = df[['song', 'track', 'duration', 'voc_oracle',
-        'instr_oracle', 'other_oracle']].dropna()
+             'instr_oracle', 'other_oracle']].dropna()
 
-    dataset = tf.data.Dataset.from_tensor_slices((tf.cast(df['song'].values, tf.string), tf.cast(df['track'].values, tf.string), tf.cast(df['duration'].values, tf.int64), tf.cast(df['voc_oracle'].values, tf.int32), tf.cast(df['instr_oracle'].values, tf.int32), tf.cast(df['other_oracle'].values, tf.int32)))
-    return dataset.map(lambda song, track, duration, voc, instr, other: (create_features(song + os.path.sep + track, duration, 1), create_label([voc, instr, other], duration, 1)))
+    data_root = data_root + os.path.sep + 'multitracks' + os.path.sep
+
+    dataset = tf.data.Dataset.from_tensor_slices((tf.cast(df['song'].values, tf.string),
+                                                  tf.cast(df['track'].values, tf.string),
+                                                  tf.cast(df['duration'].values, tf.int64),
+                                                  tf.cast(df['voc_oracle'].values, tf.int32),
+                                                  tf.cast(df['instr_oracle'].values, tf.int32),
+                                                  tf.cast(df['other_oracle'].values, tf.int32)))
+    return dataset.map(lambda song, track, duration, voc, instr, other: (
+        create_features(data_root + song + os.path.sep + track, duration, 1),
+        create_label([voc, instr, other], duration, 1))
+        ).unbatch().filter(lambda x,y: has_signal(x)).batch(4)
+
 def load_csv(filename):
-    data=pd.read_csv(filename, sep='\t')
+    data = pd.read_csv(filename, sep='\t')
     return data
+
+def load_sample(song, track, data_root, duration, chunk_size=1):
+    filename = os.path.join(data_root, 'multitracks', song, track)
+    dataset = tf.data.Dataset.from_tensors((filename, duration, chunk_size))
+    return dataset.map(lambda file, dur, c: create_features(filename, dur, c)).unbatch().filter(has_signal).batch(4)
+
+@tf.function
+def has_signal(timestep):
+    return tf.math.reduce_max(tf.abs(timestep)) > 0.05
 
 @tf.function
 def create_features(ogg_filename, duration, chunk_size):
+    wav = tfio.IOTensor.graph(tf.int16).from_audio(ogg_filename)
+    duration = tf.cast(duration, tf.int64)
+    wav = wav[0:duration*44100]  # loads whole wav data
+    wav = tf.cast(wav, tf.float32) / 32767
+    mono = tf.math.reduce_mean(wav, axis=1)  # make mono
 
-    filename= 'data' + os.path.sep + 'multitracks' + os.path.sep + ogg_filename
-    wav=tfio.IOTensor.graph(tf.int16).from_audio(filename)
-    wav=wav[0:duration*44100] # loads whole wav data
-    wav=tf.cast(wav, tf.float32) / 32767
-    mono=tf.math.reduce_mean(wav, axis=1) # make mono
-    
     # spectrogram (100 steps/sec with 1025 channels)
-    fft=tf.signal.stft(mono, 2048, 441, pad_end=True)
-    fft=tf.abs(fft)
-    spec=fft * fft
-    spec=tf.reshape(spec, [-1, 100 * chunk_size, 1025])
+    fft = tf.signal.stft(mono, 2048, 441, pad_end=True)
+    fft = tf.abs(fft)
+    spec = fft * fft
+
+    # split into timesteps (10ms * chunk_size)
+    spec = tf.reshape(spec, [-1, 100 * chunk_size, 1025])
     return spec
+
 
 @tf.function
 def create_label(y, duration, chunk_size):
     #y=tf.one_hot(tf.strings.to_number(y, tf.int32), 3)
-    y=tf.reshape(y, [-1, 3])
+    y = tf.reshape(y, [-1, 3])
     return tf.repeat(y, int(duration / chunk_size), 0)
+
 
 @tf.function
 def create_pandas_dataset(data, ignore_labels, chunk_size):
-    dataset=tf.data.Dataset.from_tensor_slices((tf.cast(data['track'].values, tf.string), tf.cast(
+    dataset = tf.data.Dataset.from_tensor_slices((tf.cast(data['track'].values, tf.string), tf.cast(
         data['duration'].values, tf.int64), tf.cast(data['ground_truth'].values, tf.string)))
     if ignore_labels:
         return dataset.map(lambda track, duration, y: get_features(track, duration, duration)).unbatch()
--- src/train.py
+++ src/train.py
@@ -1,10 +1,31 @@
 import sys
 import os
+import numpy as np
 from model import create_model
-from data import load_al_trainingset
+from data import load_al_trainingset, load_sample, load_csv
 
-dataset = load_al_trainingset(os.path.join('data','multitracks_al.txt'), 'data')
+
+
+dataset_file = os.path.join('data','multitracks_al.txt')
+data_root = 'data'
+iteration = 0
+
+
+dataset = load_al_trainingset(dataset_file , data_root)
 model = create_model()
 
+print('run training...')
 model.fit(dataset, epochs=10)
-print(dataset)
+model.save(os.path.join(f'models/al/{iteration}.h5'))
+
+print('run inference...')
+df = load_csv(dataset_file)
+for index, row in df.iterrows():
+    sample = load_sample(row['song'],row['track'], data_root, row['duration'])
+    result = np.mean(model.predict(sample),axis=0)
+
+    df.at[index, 'voc_estimated'] = result[0]
+    df.at[index, 'instr_estimated'] = result[1]
+    df.at[index, 'other_estimated'] = result[2]
+
+df.to_csv(dataset_file, sep='\t')
