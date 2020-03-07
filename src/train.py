5e76d27d16460b0e44bb3eb9948d158d40dc4783
Samuel Kupferschmid <samuel.kupferschmid@gmail.com>
2020-03-07 23:26:39 +0100

    add wandb extend model add labels

--- src/train.py
+++ src/train.py
@@ -13,24 +13,36 @@ if len(gpus) >0: tf.config.experimental.set_memory_growth(gpus[0], True)
 
 dataset_file = os.path.join('data','multitracks_al.txt')
 data_root = 'data'
-iteration = 0
 
-
+import wandb
+from wandb.keras import WandbCallback
+wandb.init(project="multitrack", tags=['voc_instr_separation','AL'])
+#config={"epochs": 10, "iteration":1}
+config = wandb.config
+config.epochs = 10
+config.iteration = 1
 dataset = load_al_trainingset(dataset_file , data_root)
 model = create_model()
 
-print('run training...')
-model.fit(dataset, epochs=10)
-model.save(os.path.join(f'models/al/{iteration}.h5'))
-
+#print('run training...')
+modelpath = os.path.join(f'models/al/{config.iteration}.h5')
+model.fit(dataset, epochs=config.epochs,callbacks=[WandbCallback()])
+model.save(modelpath)
+#model = tf.keras.models.load_model(modelpath)
 print('run inference...')
 df = load_csv(dataset_file)
 for index, row in df.iterrows():
-    sample = load_sample(row['song'],row['track'], data_root, row['duration'])
-    result = np.mean(model.predict(sample),axis=0)
+    try:
+        sample = load_sample(row['song'],row['track'], data_root, row['duration'])
+        result = np.mean(model.predict(sample),axis=0)
 
-    df.at[index, 'voc_estimated'] = result[0]
-    df.at[index, 'instr_estimated'] = result[1]
-    df.at[index, 'other_estimated'] = result[2]
+        df.at[index, 'voc_estimated'] = result[0]
+        df.at[index, 'instr_estimated'] = result[1]
+        df.at[index, 'other_estimated'] = result[2]
+    except:
+        print("Unexpected error:", sys.exc_info()[0])
+        pass
+    if index % 10 == 0: 
+        df.to_csv(dataset_file, sep='\t', index=False)
 
 df.to_csv(dataset_file, sep='\t', index=False)
